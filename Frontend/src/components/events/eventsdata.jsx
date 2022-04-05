cf0b1b78abad15cc4d905af1d5343e6b34f52b16
saikumar-27 <saiyadav27398@gmail.com>
2022-04-05 17:47:48 +0530

    Array of objects

--- Frontend/src/components/events/eventsdata.jsx
+++ Frontend/src/components/events/eventsdata.jsx
@@ -1,51 +1,58 @@
 const eventsdata = [
-    {
-      image: "https://www.bookeventz.com/blog/wp-content/uploads/2020/05/Summer-Pool-Party-Ideas.jpg",
-      id:"1",
-      title: "Pool Party",
-      subTitle: "Pool Party",
-      cost: "2400",
-      duration: "Approx 2 night trip",
-    },
-    {
-      image: "https://i.ytimg.com/vi/a9y7FEHaQVs/maxresdefault.jpg",
-      id: "2",
-      title: "Dance Concert",
-      subTitle: "Dance Concert",
-      cost: "5,200",
-      duration: "Approx 2 night trip",
-    },
-    {
-      image: "https://media.istockphoto.com/photos/cheering-crowd-of-unrecognized-people-at-a-rock-music-concert-concert-picture-id1189205501?k=20&m=1189205501&s=612x612&w=0&h=fexl_Cmu6AdLatIasGg_XYTkLSeWHCtvhMw1nK5_uDc=",
-      id: "3",
-      title: "Music Concert",
-      subTitle: "Music Concert At Pune",
-      cost: "45,500",
-      duration: "Approx 2 night trip",
-    },
-    {
-      image: "https://4.imimg.com/data4/FA/NN/MY-12958090/dj-service-500x500.jpeg",
-      id: "4",
-      title: "Dj Party",
-      subTitle: "Exhibition",
-      cost: "24,100",
-      duration: "Approx 1 night trip",
-    },
-    {
-      image: "http://s3.amazonaws.com/busites_www/kissonline/content/articles/newcastle.jpg",
-      id: "5",
-      title: "Band Concert",
-      subTitle: "Bora Bora is a small South Pacific island northwest of",
-      cost: "95,400",
-      duration: "Approx 2 night 2 day trip",
-    },
-    {
-      image: "https://wallup.net/wp-content/uploads/2019/10/86222-kataklysm-death-metal-heavy-hard-rock-concert-concerts-guitar-guitars-1-748x499.jpg",
-      id: "6",
-      title: "Rock Concert",
-      subTitle: "London, the capital of England and the United",
-      cost: "38,800",
-      duration: "Approx 3 night 2 day trip",
-    },
-  ]
-  export default eventsdata;
+  {
+    image:
+      "https://www.bookeventz.com/blog/wp-content/uploads/2020/05/Summer-Pool-Party-Ideas.jpg",
+    id: "1",
+    title: "Pool Party",
+    subTitle: "Let’s Splash the day",
+    cost: "7,400",
+    location: "📍  Sundakapatty, Bangalore",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    id: "2",
+    title: "Dance Concert",
+    subTitle: "Dance Your Feet To The Broadway Beat",
+    cost: "5,200",
+    location: "📍 Kompally, Hyderabad",
+  },
+  {
+    image:
+      "https://media.istockphoto.com/photos/cheering-crowd-of-unrecognized-people-at-a-rock-music-concert-concert-picture-id1189205501?k=20&m=1189205501&s=612x612&w=0&h=fexl_Cmu6AdLatIasGg_XYTkLSeWHCtvhMw1nK5_uDc=",
+    id: "3",
+    title: "Music Concert",
+    subTitle: "Let's go where the music is too loud.",
+    cost: "5,500",
+    location: "📍 Chattarpur, Delhi",
+  },
+  {
+    image:
+      "https://4.imimg.com/data4/FA/NN/MY-12958090/dj-service-500x500.jpeg",
+    id: "4",
+    title: "Dj Party",
+    subTitle: "DJ Player : Aseem Rajpal",
+    cost: "8,100",
+    location: " 📍 Kapashera, Delhi",
+  },
+  {
+    image:
+      "https://media.istockphoto.com/photos/drummer-playing-drums-on-stage-picture-id1148625127?b=1&k=20&m=1148625127&s=170667a&w=0&h=ayd1Cea7Oik-ZHHMIKGvbgPXvMG-naMGsTSJEC5N_YA=",
+    id: "5",
+    title: "Band Concert",
+    subTitle: "Let the sea resound",
+    cost: "6,400",
+    location: "📍 Siddapura, Bangalore",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1611898979774-e202e8e9ffbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cm9jayUyMGNvbmNlcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    id: "6",
+    title: "Rock Concert",
+    subTitle: "Rock concerts are the churches of today",
+    cost: "7,800",
+    location: "📍 Sector 135, Noida",
+  },
+];
+export default eventsdata;
