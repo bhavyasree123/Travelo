e444530e39c88c5d38935780f191e5864a08b45a
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-06 13:04:25 +0530

    Updated Events.

--- Frontend/src/components/events/specificpage/ModalData.jsx
+++ Frontend/src/components/events/specificpage/ModalData.jsx
@@ -9,34 +9,82 @@ import {
   Select,
   TextField,
   Typography,
-} from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DateTimePicker from "@mui/lab/DateTimePicker";
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DateTimePicker from '@mui/lab/DateTimePicker';
 
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 800,
   height: 630,
-  bgcolor: "background.paper",
-  borderRadius: "4%",
+  bgcolor: 'background.paper',
+  borderRadius: '4%',
   boxShadow: 24,
   p: 4,
 };
 
 export default function ModalData({ open, handleClose, data }) {
-  const [venue, setVenue] = React.useState("");
-  const [value, setValue] = React.useState(new Date());
+  // Form Data
+  const [venue, setVenue] = React.useState('');
+  const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+  const [eventName, setEventName] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [title, setTitle] = useState('');
 
-  const handleChange = (event) => {
-    setVenue(event.target.value);
+  useEffect(() => {
+    const title = JSON.parse(localStorage.getItem('venue'));
+
+    setTitle(title?.title);
+    // console.log(title);
+  }, [title, name]);
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+    // console.log(newValue);
   };
+
+  const handleCity = (venue) => {
+    setVenue(venue.target.value);
+    // console.log(venue.target.value);
+  };
+
+  const history = useHistory();
+
+  //  Get the data from form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const obj = {
+      eventName: title,
+      dateAndTime: value,
+      city: venue,
+      name: name,
+      email: email,
+      phone: phone,
+    };
+
+    console.log(obj);
+    const res = await axios.post('/regEvent', obj);
+
+    if (res) {
+      console.log('Data Submittted');
+
+      setTimeout(() => {
+        history.push('/Adventures');
+      }, 2000);
+    }
+  };
+
   return (
     <Modal
       open={open}
@@ -48,11 +96,11 @@ export default function ModalData({ open, handleClose, data }) {
         <button
           onClick={handleClose}
           style={{
-            float: "right",
-            padding: "10px 15px",
-            backgroundColor: "white",
-            border: "none",
-            cursor: "pointer",
+            float: 'right',
+            padding: '10px 15px',
+            backgroundColor: 'white',
+            border: 'none',
+            cursor: 'pointer',
           }}
         >
           <CloseIcon />
@@ -70,7 +118,7 @@ export default function ModalData({ open, handleClose, data }) {
             <br></br>
 
             <img
-              style={{ width: 350, borderSpacing: "0 10px" }}
+              style={{ width: 350, borderSpacing: '0 10px' }}
               src={data.image}
               id="modal-modal-title"
               variant="h6"
@@ -82,55 +130,78 @@ export default function ModalData({ open, handleClose, data }) {
               {data.location}
             </Typography>
           </Grid>
-          <Grid item style={{ paddingRight: "90px" }}>
-            <h2>Get a Quick quote</h2>
-            <br></br>
-            <br></br>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DateTimePicker
-                renderInput={(props) => <TextField {...props} />}
-                label="Select Date and Time"
-                minDate={new Date()}
-                inputFormat="dd/MM/yyyy   hh:mm  a"
-                value={value}
-                onChange={(newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </LocalizationProvider>
-            <br></br>
-            <br></br>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  --Select City--
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={venue}
-                  label="Age"
+
+          {/* Form */}
+          <form action="#">
+            <Grid item style={{ paddingRight: '90px' }}>
+              <h2>Get a Quick quote</h2>
+              <br></br>
+              <br></br>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                  renderInput={(props) => <TextField {...props} />}
+                  label="Select Date and Time"
+                  minDate={new Date()}
+                  inputFormat="dd/MM/yyyy   hh:mm  a"
+                  value={value}
                   onChange={handleChange}
-                >
-                  <MenuItem value={1}>Hyderabad</MenuItem>
-                  <MenuItem value={2}>Bangalore</MenuItem>
-                  <MenuItem value={3}>Chennai</MenuItem>
-                  <MenuItem value={4}>Delhi</MenuItem>
-                  <MenuItem value={5}>Mumbai</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-            <br></br>
-            <TextField type="name" label="Enter your Name"></TextField>
-            <br></br>
-            <br></br>
-            <TextField type="email" label="Email"></TextField> <br></br>
-            <br></br>
-            <TextField type="tel" label="Mobile no"></TextField> <br></br>
-            <br></br>
-            <br></br>
-            <Button variant="contained">Submit</Button>
-          </Grid>
+                />
+              </LocalizationProvider>
+              <br></br>
+              <br></br>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    --Select City--
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={venue}
+                    label="Age"
+                    onChange={handleCity}
+                  >
+                    <MenuItem value="Hyderabad">Hyderabad</MenuItem>
+                    <MenuItem value="Bangalore">Bangalore</MenuItem>
+                    <MenuItem value="Chennai">Chennai</MenuItem>
+                    <MenuItem value="Delhi">Delhi</MenuItem>
+                    <MenuItem value="Mumbai">Mumbai</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <br></br>
+              <TextField
+                type="name"
+                label="Enter your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></TextField>
+              <br></br>
+              <br></br>
+              <TextField
+                type="email"
+                label="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></TextField>{' '}
+              <br></br>
+              <br></br>
+              <TextField
+                type="tel"
+                label="Mobile no"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              ></TextField>{' '}
+              <br></br>
+              <br></br>
+              <br></br>
+              <Button variant="contained" onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Grid>
+
+            {/* Form End */}
+          </form>
         </Grid>
       </Box>
     </Modal>
