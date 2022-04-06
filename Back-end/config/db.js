d7bd3bcad0916dc0dd5a60c9eeab74b4261b5235
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2022-04-06 18:59:14 +0530

    updated

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const mongoConnect = async () => {
   const connection = await mongoose.connect(process.env.MONGO_URL, {
       dbName: 'TRAVELO',
+      useNewUrlParser : true,
+      useUnifiedTopology: true
   }).then(()=> {
       console.log("connnected to MongoDB Successfully")
   });
@@ -10,5 +12,3 @@ const mongoConnect = async () => {
 };
 
 module.exports = mongoConnect;
-
-
