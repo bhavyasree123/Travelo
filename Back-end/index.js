54abd149433c0b7e2775b1f6ac527f69fa714fa7
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2022-04-07 14:22:27 +0530

    updated fix

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,31 +2,28 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const mongoDb = require('./config/db');
-const userRoute = require("./models/User");
+const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/booked");
-const { application } = require('express');
+
 
 const app = express();
 
 dotenv.config();
 const  PORT  = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(express.json()); //express.json() is a built-in middleware function in Express. This method is used to parse the incoming requests with JSON payloads and is based upon the bodyparser.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 mongoDb();
-
-
 dotenv.config();
 
 
-
-app.use(cors());
-app.use(express.json());
+//Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
@@ -39,7 +36,11 @@ app.use('/', require('./routes/events'));
 // app.use('/posts', require('./routes/posts'));
 
 app.get('/',(req,res)=>{
-  res.send("Welcome to the server");
+  res.send("Welcome to the G2G");
+});
+
+app.get('/1',(req,res)=>{
+  res.send("Welcome to Food Floor");
 });
 
 app.listen(PORT, () => {
